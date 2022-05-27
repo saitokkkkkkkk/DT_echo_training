1abf02eb6ed423b18048b8edc91453e09677be39
SeiyaNakamura <s_nakamura@craid-inc.com>
2022-05-27 09:41:04 +0900

    fix db connection

--- app/infrastructure/sqlhandler/sqlhandler.go
+++ app/infrastructure/sqlhandler/sqlhandler.go
@@ -1,7 +1,9 @@
 package sqlhandler
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -13,10 +15,14 @@ type SqlHandler struct {
 
 func NewSqlHandler() *SqlHandler {
 	sqlHandler := new(SqlHandler)
-	dsn := "root:deeptrack@tcp(mysql-container:3306)/DeepTrack?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	dbname := os.Getenv("MYSQL_DATABASE")
+	connection := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pass, host, dbname)
+	DB, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
 	if err != nil {
-		log.Fatalln(dsn + "database can't connect")
+		log.Fatalln(connection + "database can't connect")
 	}
 	sqlHandler.DB = DB
 	return sqlHandler
